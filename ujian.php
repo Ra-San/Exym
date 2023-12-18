6ad17beedccc1a6832f9ebe0bb59a4d84c797d93
xero <chansenku0@gmail.com>
2023-12-18 11:44:21 +0700

    ujian

---
+++ ujian.php
@@ -0,0 +1,21 @@
+<?php
+echo "<p>Saya mengerjakan ujian ini dengan jujur!</p>";
+
+echo "<img src='gambar.jpg' alt='Deskripsi gambar'>";
+
+echo "<h2>Programming</h2>";
+/*
+"sedang mengerjakan soal!"
+"dilarang berisik, ada ujian"
+"tetap fokus dan konsentrasi saat belajar, hindari gangguan dan periksa jadwal secara rutin"
+*/
+
+echo "<textarea placeholder='sedang mengerjakan soal!' ></textarea>";
+
+echo "<style>
+p {
+    font-size: 50%; 
+    color: green;
+   }
+</style>";
+?>
